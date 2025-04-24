841a7cd4388193ae6f2fe9172dc99c4b2766af4d
AyushiAnand70047 <ayushianand088@gmail.com>
2025-04-24 11:24:33 +0530

    break use

---
+++ breakQues.m
@@ -0,0 +1,9 @@
+# write a MATLAB Script to iterate number from 1 to 20 and performs the following task
+# 1. stop the loop if the numbers reaches upto 15
+
+for i = 1:20
+  if i > 15
+    break;
+  end
+  disp(i);
+end
